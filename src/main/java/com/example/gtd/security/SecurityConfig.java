ae1d3f2a914e94d6296ae65871ecf0418203cd37
rodrigo <rodrigo@chavez.com>
2025-05-27 21:56:56 -0300

    hace publicas las peticiones POST a /api/usuarios

--- src/main/java/com/example/gtd/security/SecurityConfig.java
+++ src/main/java/com/example/gtd/security/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
       .csrf(csrf -> csrf.disable())
       .httpBasic(withDefaults())
       .authorizeHttpRequests(auth -> auth
+        .requestMatchers(HttpMethod.POST, "/api/usuarios").permitAll()
         .anyRequest().authenticated()
       );
     return http.build();
