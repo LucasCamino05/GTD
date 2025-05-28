b0eec5a86ade2f1fcf07949223996d917875f4a1
rodrigo <rodrigo@chavez.com>
2025-05-27 22:13:43 -0300

    agregar ejemplos para crear usuarios y su perfil

--- src/main/java/com/example/gtd/GtdApplication.java
+++ src/main/java/com/example/gtd/GtdApplication.java
@@ -5,9 +5,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GtdApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GtdApplication.class, args);
+    /*
+    # crear usuario
+    curl -X POST http://localhost:8080/api/usuarios \
+    -H "Content-Type: application/json" \
+    -d '{"nombre": "juan_perez", "contrasena": "clave", "rol": "DUENO"}'
+    
+    # crear perfil al usuario
+    curl -u usuario1:clave -X POST http://localhost:8080/api/perfiles/dueno \
+    -H "Content-Type: application/json" \
+    -d '{
+      "nombre": "Juan",
+      "apellido": "Perez",
+      "telefono": "123456789",
+      "correo": "juan@example.com",
+      "cvu": "1234567890123456789012"
+    }'
+     */
 	}
 
 }
