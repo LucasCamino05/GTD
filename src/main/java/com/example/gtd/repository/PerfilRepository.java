e83710b59cd93dc5fdbcfae915b5da1704fde345
rodrigo <rodrigo@chavez.com>
2025-05-27 21:38:04 -0300

    crea repositorio de perfiles

---
+++ src/main/java/com/example/gtd/repository/PerfilRepository.java
@@ -0,0 +1,8 @@
+package com.example.gtd.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.gtd.model.Perfil;
+
+public interface PerfilRepository extends JpaRepository<Perfil, Long> {
+}
