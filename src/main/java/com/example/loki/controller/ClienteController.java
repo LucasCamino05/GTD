1eb2469c6320f74f43ee50d7bcefe7f206e781c3
rodrigo <rodrigo@chavez.com>
2025-05-31 19:48:28 -0300

    crea cliente controller

---
+++ src/main/java/com/example/loki/controller/ClienteController.java
@@ -0,0 +1,24 @@
+package com.example.loki.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.loki.model.Cliente;
+import com.example.loki.repository.PerfilRepository;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+  @Autowired
+  private PerfilRepository repositorio;
+
+  @PostMapping
+  public ResponseEntity<?> crear(@RequestBody Cliente cliente) {
+    repositorio.save(cliente);
+    return ResponseEntity.ok("cliente creado");
+  }
+}
