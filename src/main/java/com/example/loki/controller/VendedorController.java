e217b290244c0683c5638a14747adcabb57184d9
rodrigo <rodrigo@chavez.com>
2025-05-31 19:51:55 -0300

    crea vendedor controller

---
+++ src/main/java/com/example/loki/controller/VendedorController.java
@@ -0,0 +1,24 @@
+package com.example.loki.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.loki.model.Vendedor;
+import com.example.loki.repository.PerfilRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/vendedores")
+public class VendedorController {
+  @Autowired
+  private PerfilRepository repositorio;
+
+  @PostMapping
+  public ResponseEntity<?> crear(@RequestBody Vendedor vendedor) {
+    repositorio.save(vendedor);
+    return ResponseEntity.ok("Vendedor creado");
+  }
+}
