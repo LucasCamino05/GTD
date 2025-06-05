bf2dce5841a9998db76a1987f0ac4234e872d11e
rodrigo <rodrigo@chavez.com>
2025-06-05 18:47:35 -0300

    implement endpoint to modify Vendedor
    - add 'PUT /api/vendedores/{id}' endpoint
    - example curl request
    
      ```
      curl -X PUT http://localhost:8080/api/vendedores/1 \
           -H "Content-Type: application/json" \
           -d '{
                 "alias": "alias-123"
               }'
    
      ```
      that edit the 'alias' field of vendedor with '1' as id
      to 'alias-123'

--- src/main/java/com/example/loki/controller/VendedorController.java
+++ src/main/java/com/example/loki/controller/VendedorController.java
@@ -3,12 +3,17 @@ package com.example.loki.controller;
 import com.example.loki.exceptions.PerfilNotFound;
 import com.example.loki.model.Vendedor;
 import com.example.loki.service.VendedorService;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/vendedores")
@@ -16,18 +21,38 @@ public class VendedorController {
 
   @Autowired
   private VendedorService vendedorService;
+  @Autowired
+  private ObjectMapper objectMapper;
 
   @GetMapping
-  public List<String> getVendedor(){
+  public List<String> getVendedor() {
     return vendedorService.getVendedor()
-            .stream().map(x -> x.toString())
-            .toList();
+        .stream().map(x -> x.toString())
+        .toList();
   }
+
   @GetMapping("/{id}")
-  public String getVendedorById(@PathVariable Long id) throws PerfilNotFound{
+  public String getVendedorById(@PathVariable Long id) throws PerfilNotFound {
     return vendedorService.getVendedorById(id)
-            .orElseThrow(() -> new PerfilNotFound("Usuario no encontrado"))
-            .toString();
+        .orElseThrow(() -> new PerfilNotFound("Usuario no encontrado"))
+        .toString();
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<?> modificar(@PathVariable Long id, @RequestBody Map<String, Object> modificacion) throws PerfilNotFound {
+    Optional<Vendedor> vendedor = vendedorService.getVendedorById(id);
+    if (vendedor.isPresent()) {
+      try {
+        objectMapper.updateValue(vendedor.get(), modificacion);
+      } catch (JsonMappingException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+      }
+      vendedorService.saveVendedor(vendedor.get());
+      return ResponseEntity.ok("Vendedor modificado");
+    } else {
+      throw new PerfilNotFound("Usuario no encontrado");
+      // return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Vendedor no encontrado");
+    }
   }
 
   @PostMapping
@@ -35,13 +60,15 @@ public class VendedorController {
     vendedorService.saveVendedor(vendedor);
     return ResponseEntity.ok("Vendedor creado");
   }
+
   @DeleteMapping("/{id}")
-  public ResponseEntity<?> eliminarVendedor(@PathVariable Long id) throws PerfilNotFound{
+  public ResponseEntity<?> eliminarVendedor(@PathVariable Long id) throws PerfilNotFound {
     vendedorService.deleteVendedor(id);
     return ResponseEntity.ok("Vendedor eliminado");
   }
+
   @ExceptionHandler
-  public ResponseEntity<String> manejadorPerfilNotFound(PerfilNotFound ex){
+  public ResponseEntity<String> manejadorPerfilNotFound(PerfilNotFound ex) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
   }
 }
