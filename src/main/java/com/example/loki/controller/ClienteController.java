779014b381879823dee05e1a36770c622b661cda
rodrigo <rodrigo@chavez.com>
2025-06-14 17:25:26 -0300

    valida cliente antes de crear (cliente controller)

--- src/main/java/com/example/loki/controller/ClienteController.java
+++ src/main/java/com/example/loki/controller/ClienteController.java
@@ -3,8 +3,10 @@ package com.example.loki.controller;
 import com.example.loki.exceptions.PerfilNotFound;
 import com.example.loki.model.Cliente;
 import com.example.loki.service.ClienteService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,29 +20,26 @@ public class ClienteController {
   private ClienteService clienteService;
 
   @GetMapping
-  public List<String> getCliente(){
+  public List<String> getCliente() {
     return clienteService.getCliente()
-            .stream().map(x-> x.toString())
-            .toList();
+        .stream().map(x -> x.toString())
+        .toList();
   }
+
   @GetMapping("/{id}")
-  public Cliente getClienteById(@PathVariable Long id) throws PerfilNotFound{
+  public Cliente getClienteById(@PathVariable Long id) throws PerfilNotFound {
     return clienteService.getClienteById(id).orElseThrow(() -> new PerfilNotFound("Usuario no encontrado"));
   }
 
   @PostMapping
-  public ResponseEntity<?> crear(@RequestBody Cliente cliente) {
+  public ResponseEntity<?> crear(@RequestBody @Valid Cliente cliente) {
     clienteService.saveCliente(cliente);
     return ResponseEntity.ok("cliente creado");
   }
 
   @DeleteMapping("/{id}")
-  public ResponseEntity<?> eliminarCliente(@PathVariable Long id) throws PerfilNotFound{
+  public ResponseEntity<?> eliminarCliente(@PathVariable Long id) throws PerfilNotFound {
     clienteService.deleteCliente(id);
     return ResponseEntity.ok("Eliminado con exito!");
-  }
-  @ExceptionHandler
-  public ResponseEntity<String> manejadorPerfilNotFound(PerfilNotFound ex){
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
   }
 }
