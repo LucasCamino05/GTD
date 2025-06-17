0f7602d0d82f2aba21134fd5f6e3b1a6c8fb0ee5
Lucas Camino <lucascamino05@gmail.com>
2025-06-17 13:45:44 -0300

    Implementacion de Aceptar y Rechazar ofertas en el Service.

--- src/main/java/com/example/loki/controller/OfertaController.java
+++ src/main/java/com/example/loki/controller/OfertaController.java
@@ -1,9 +1,13 @@
 package com.example.loki.controller;
 
+import com.example.loki.model.entities.Perfil;
+import com.example.loki.exceptions.NotFoundException;
 import com.example.loki.service.OfertaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("loki/v1/oferta")
@@ -13,5 +17,16 @@ public class OfertaController{
     @Autowired
     public OfertaController(OfertaServiceImpl ofertaService){
         this.ofertaService = ofertaService;
+    }
+
+    @PutMapping("/{id}/aceptar")
+    public ResponseEntity<?> actualizarEstado(@PathVariable Long id) throws NotFoundException {
+        try {
+            Perfil perfil = (Perfil) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            ofertaService.aceptarOferta(id, perfil);
+        }catch (NotFoundException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Actualizado con exito!");
     }
 }
--- src/main/java/com/example/loki/service/OfertaServiceImpl.java
+++ src/main/java/com/example/loki/service/OfertaServiceImpl.java
@@ -1,8 +1,15 @@
 package com.example.loki.service;
 
+import com.example.loki.exceptions.NotFoundException;
+import com.example.loki.model.entities.Cliente;
 import com.example.loki.model.entities.Oferta;
+import com.example.loki.model.entities.Perfil;
+import com.example.loki.model.entities.Vendedor;
+import com.example.loki.model.enums.EstadoOferta;
 import com.example.loki.repository.OfertaRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AuthorizationServiceException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -39,5 +46,35 @@ public class OfertaServiceImpl implements OfertaService{
     @Override
     public void updateOferta(Long id, Double nuevoPrecio) {
 
+    }
+
+    @Transactional
+    public Oferta aceptarOferta(Long ofertaID, Perfil perfil) throws NotFoundException, AuthorizationServiceException {
+        Oferta oferta = repository.findById(ofertaID)
+                .orElseThrow(() -> new NotFoundException("No se encontro ninguna oferta."));
+        if(perfil instanceof Cliente && oferta.getCliente().getId().equals(perfil.getId())){
+            oferta.setEstado(EstadoOferta.ACEPTADA);
+        } else if (perfil instanceof Vendedor && oferta.getVendedor().getId().equals(perfil.getId())) {
+            oferta.setEstado(EstadoOferta.ACEPTADA);
+        }else{
+            throw new AuthorizationServiceException("No es posible aceptar la oferta.");
+        }
+
+        return repository.save(oferta);
+    }
+
+    @Transactional
+    public Oferta rechazarOferta(Long ofertaID, Perfil perfil) throws NotFoundException, AuthorizationServiceException {
+        Oferta oferta = repository.findById(ofertaID)
+                .orElseThrow(() -> new NotFoundException("No se encontro ninguna oferta."));
+        if(perfil instanceof Cliente && oferta.getCliente().getId().equals(perfil.getId())){
+            oferta.setEstado(EstadoOferta.CANCELADA);
+        } else if (perfil instanceof Vendedor && oferta.getVendedor().getId().equals(perfil.getId())) {
+            oferta.setEstado(EstadoOferta.CANCELADA);
+        }else{
+            throw new AuthorizationServiceException("No es posible aceptar la oferta.");
+        }
+
+        return repository.save(oferta);
     }
 }
